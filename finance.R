655d6ea84b4b2ab5c70329d6f9e5cc88fea43cd9
anthonyjp87 <john.a.perez@gmail.com>
2017-03-06 15:34:17 +0000

    update

--- finance.R
+++ finance.R
@@ -1,11 +1,13 @@
 #get Dates in dd-mm-yyyy format
 #remove , separator from numbers
 USDXGBP <- 1.25
+per <- 0.01
 df <- read.csv('finance.csv')
 df$Date <- as.Date(df$Date, format = "%d/%m/%Y")
 df$Cost <- as.numeric(as.character(df$Cost))
 df$Cost<-ifelse(df$Currency=="S",df$Cost,df$Cost/USDXGBP)
 dfMonth<-df[df$Date >= "2017-2-1" & df$Date <= "2017-3-1",]
-quant_10<-quantile(dfMonth$Cost, 0.1,na.rm=TRUE)
-top_10per_transactions<-subset(dfMonth, dfMonth$Cost < quant_10)
-print(top_10per_transactions)
+quant<-quantile(dfMonth$Cost, per ,na.rm=TRUE)
+top_transactions<-subset(dfMonth, dfMonth$Cost < quant)
+print(top_transactions)
+
