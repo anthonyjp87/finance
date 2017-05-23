5b113a1a0c0867be93f7a32e0a371c64761eaafd
anthonyjp87 <john.a.perez@gmail.com>
2017-05-23 16:11:32 +0100

    updated to use system date

--- finance.R
+++ finance.R
@@ -1,13 +1,15 @@
 #get Dates in dd-mm-yyyy format
 #remove , separator from numbers
+setwd("~/Documents/R_Files/finance")
 USDXGBP <- 1.25
-per <- 0.01
+per <- 0.2
+start_date <-Sys.Date()-30
+end_date <-Sys.Date()
 df <- read.csv('finance.csv')
 df$Date <- as.Date(df$Date, format = "%d/%m/%Y")
 df$Cost <- as.numeric(as.character(df$Cost))
 df$Cost<-ifelse(df$Currency=="S",df$Cost,df$Cost/USDXGBP)
-dfMonth<-df[df$Date >= "2017-2-1" & df$Date <= "2017-3-1",]
+dfMonth<-df[df$Date >= start_date & df$Date <= end_date,]
 quant<-quantile(dfMonth$Cost, per ,na.rm=TRUE)
 top_transactions<-subset(dfMonth, dfMonth$Cost < quant)
 print(top_transactions)
-
